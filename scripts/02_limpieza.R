e8aab9368fb879a9b09c512cff8ce46fbf8a1d93
Rebmob22 <nachorolon11@gmail.com>
2025-11-23 21:06:28 -0300

    Reduccion de base
    Optimizacion de bases

--- scripts/02_limpieza.R
+++ scripts/02_limpieza.R
@@ -25,7 +25,7 @@ df_prod_potencial_limpio <- limpiar_prop(df_prod_potencial)
 # Es buena práctica agregar un timestamping o sufijo claro (ej: _limpio)
 
 ruta_salida_export <- here("data", "processed", "df_prod_export_limpio.rds")
-ruta_salida_potencial <- here("data", "processed", "df_prod_pontecial_limpio.rds")
+ruta_salida_potencial <- here("data", "processed", "df_prod_potencial_limpio.rds")
 
 saveRDS(df_prod_export_limpio, file = ruta_salida_export)
 saveRDS(df_prod_potencial_limpio, file = ruta_salida_potencial)
@@ -34,3 +34,29 @@ saveRDS(df_prod_potencial_limpio, file = ruta_salida_potencial)
 cat("\nDatos procesados guardados con éxito:",
     "\n ->", ruta_salida_export,
     "\n ->", ruta_salida_potencial, "\n")
+
+# ==============================================================================
+# REDUCCION DE COLUMNAS INUTILES PARA EL PROYECTO
+# ------------------------------------------------------------------------------
+
+df_prod_export_reducido <- df_prod_export_limpio %>% 
+  select(-cod_provincia, -cod_ncm_6d)
+
+df_prod_potencial_reducido <- df_prod_potencial_limpio %>%
+  select(-cod_provincia, -cod_ncm_6d, -fob_mundial, -ranking_est_conservadora, -ranking_est_equilibrada, -ranking_est_desafiante)
+
+# 5. Guardar los datos procesados
+
+# Se utiliza la carpeta data/processed/ para los resultados de la limpieza y transformación [3].
+# Es buena práctica agregar un timestamping o sufijo claro (ej: _reducido)
+
+ruta_salida_export_red <- here("data", "clean", "df_prod_export_reducido.rds")
+ruta_salida_potencial_red <- here("data", "clean", "df_prod_potencial_reducido.rds")
+
+saveRDS(df_prod_export_reducido, file = ruta_salida_export_red)
+saveRDS(df_prod_potencial_reducido, file = ruta_salida_potencial_red)
+
+# Documentación (Logging y documentación automática)
+cat("\nDatos procesados guardados con éxito:",
+    "\n ->", ruta_salida_export_red,
+    "\n ->", ruta_salida_potencial_red, "\n")
